fe99e4a47cb3805e848d0932e28976a6c4b067ef
nikita Agusha <nikita.a1999@mail.ru>
2020-07-03 11:22:34 +0600

    Реализовано на реакт

--- src/components/BlockSearch/BlockSearch.js
+++ src/components/BlockSearch/BlockSearch.js
@@ -8,6 +8,7 @@ const BlockSearch =()=> {
             <div style={{borderBottom: "2px solid #d7e2f1",  width: "70px", background: "#f5f7fa", paddingTop:"10vh"}}>
                 <img src={require("../../img/bx-search.svg")} className="search_img" alt=""/>
                 <p className="info_search">Поиск</p>
+
                 <span className="search_border_bottom"/>
             </div>
         </div>
