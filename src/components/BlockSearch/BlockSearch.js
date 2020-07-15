68ecbd4a8c2f81d2e940078c2b1ac72164c95332
Serik Serikbay <bsserikbay@gmail.com>
2020-07-15 23:19:54 +0600

    commit message in here

--- src/components/BlockSearch/BlockSearch.js
+++ src/components/BlockSearch/BlockSearch.js
@@ -8,7 +8,6 @@ const BlockSearch =()=> {
             <div className="blockSearch">
                 <img src={require("../../img/bx-search.svg")} className="search_img" alt=""/>
                 <p className="info_search">Поиск</p>
-
                 <span className="search_border_bottom"/>
             </div>
         </div>
