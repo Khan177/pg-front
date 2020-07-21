2ea4a105a400afc48d09f841ca17c91b76572867
nikita Agusha <nikita.a1999@mail.ru>
2020-07-21 18:07:13 +0600

    убрал компонент AllHeader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
 import './assets/fonts/sf-ui-display-cufonfonts-webfont/style.css'
-import AllHeader from "./containers/AllHeader/AllHeader";
 import Content from "./containers/PartnerTable/Content";
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import PartnerEdit from "./containers/PartnerEdit/PartnerEdit";
 import OutdoorFurniture from "./containers/Base/OutdoorFurniture/OudoorFurniture";
+import Header from "./components/Header/Header";
 function App() {
     return (
         <>
             <BrowserRouter>
                 <header>
-                    <AllHeader/>
+                    <Header/>
                 </header>
                 <Switch>
                     <Route path="/base/partners" exact component={Content}/>
                     {/*<Route path="" exact component={newFilter}/>*/}
-                    <Route path="/partnerEdit" exact component={PartnerEdit}/>
+                    <Route path="/base/partner/info" exact component={PartnerEdit}/>
                     <Route path="/outdoor_furniture" exact component={OutdoorFurniture}/>
                 </Switch>
             </BrowserRouter>
